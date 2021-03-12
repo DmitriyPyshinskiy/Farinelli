14648d1802b69a41101ba0d5c5eeafdced3713a9
Dmitriy Pyshinskiy <dmitriy.pyshinskiy@mail.ru>
2021-03-13 01:11:30 +0300

    initial commit

---
+++ src/main/java/ru/pyshsoft/farinelli/FarinelliApplication.java
@@ -0,0 +1,13 @@
+package ru.pyshsoft.farinelli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarinelliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarinelliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/pyshsoft/farinelli/FarinelliApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.pyshsoft.farinelli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarinelliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
